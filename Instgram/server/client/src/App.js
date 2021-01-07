359cc720f525c0a4de97011b2ab80ac6557b43ff
karthik <kamathkarthik13@gmail.com>
2021-01-07 14:06:20 +0530

    done

---
+++ Instgram/server/client/src/App.js
@@ -0,0 +1,60 @@
+import React,{useEffect,createContext,useReducer,useContext} from 'react'
+import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter,Route, Switch, useHistory} from "react-router-dom"
+import Home from './components/screens/Home';
+
+import Signup from './components/screens/Signup';
+import Profile from './components/screens/Profile';
+import CreatePost from './components/screens/CreatePost';
+import { initialState, reducer } from './reducers/userReducer';
+import UserProfile from './components/screens/UserProfile';
+import SubscribesUserPost from './components/screens/SubscribesUserPost';
+import Signin from './components/screens/Signin';
+
+
+export const UserContext = createContext()
+
+const Routing = ()=>{
+  const history=useHistory()
+  const {state,dispatch}=useContext(UserContext)
+  
+  useEffect(()=>{
+
+    const user=JSON.parse(localStorage.getItem("user"))
+    
+    if(user){
+      dispatch({type:"USER",playload:user})
+    }else{
+      history.push("/signin")
+    }
+  },[])
+  return (
+    <Switch>
+      <Route  exact path='/' component={Home}/>
+      <Route   path='/signin' component={Signin}/>
+      <Route  path="/signup" component={Signup}/>
+      <Route exact path="/profile" component={Profile}/>
+      <Route  path="/create" component={CreatePost}/>
+      <Route  path="/profile/:userid" component={UserProfile}/>
+      <Route  path="/myfollowingpost" component={SubscribesUserPost}/>
+    </Switch>
+  )
+
+}
+
+function App() {
+  const [state,dispatch]=useReducer(reducer,initialState)
+  return (
+    <UserContext.Provider value={{state,dispatch}}>
+    <BrowserRouter>
+    <div className="App">
+      <Navbar/>
+      <Routing/>
+    </div>
+    </BrowserRouter>
+    </UserContext.Provider>
+  )
+}
+
+export default App;
